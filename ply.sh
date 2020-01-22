df67813b54f09ada75f1fc768723bc0b8c374bde
Eslam karim gaber <eslamkarim222@gmail.com>
2020-01-22 18:42:27 +0200

    Create ply.sh

---
+++ ply.sh
@@ -0,0 +1,22 @@
+clear;
+echo "Welcome to PLY database engine. Please Select an option";
+select opt in "Create Database" "Use DataBase" "List Databases and Tables";
+do
+    case $opt in
+        "Create Database" )
+            echo "create DB"
+            break
+            ;;
+        "Use DataBase" )
+            echo "use dB"
+            break
+            ;;
+        "List Databases and Tables" )
+            echo "list db and tables"
+            break
+            ;;
+        * )
+            echo "Wrong Selection"
+            ;;
+    esac
+done
