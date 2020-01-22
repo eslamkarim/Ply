7b174139f05c5ea0eba228d22c5ab55fc3ddb13e
Eslam karim gaber <eslamkarim222@gmail.com>
2020-01-22 19:25:05 +0200

    creating directories if db doesn't exist

--- createDB.sh
+++ createDB.sh
@@ -4,8 +4,17 @@ clear;
 createDB(){
     if [[ -d "/c/Users/Eslam/Desktop/ITI/Bash/Ply/ply" ]]
     then
-        echo "$DIRECTORY exists on your filesystem."
+        echo "Please enter the Database name to create"
+        read dbname
+        if [[ -d "/c/Users/Eslam/Desktop/ITI/Bash/Ply/ply/$dbname" ]]
+        then
+            "sorry Database already exists"
+        else
+            mkdir /c/Users/Eslam/Desktop/ITI/Bash/Ply/ply/$dbname
+            echo "Database Created"
+        fi
     else
         echo "making the dir"
+        mkdir ply
     fi
 } 
