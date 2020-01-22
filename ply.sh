f244abcdfd9b6055d3a7a1f326133705db696955
Eslam karim gaber <eslamkarim222@gmail.com>
2020-01-22 19:36:56 +0200

    adding exit and prepare to call the entry point from different functionalities

--- ply.sh
+++ ply.sh
@@ -2,25 +2,35 @@
 
 clear;
 . ./createDB.sh
-echo "Welcome to PLY database engine. Please Select an option";
-select opt in "Create Database" "Use DataBase" "List Databases and Tables";
-do
-    case $opt in
-        "Create Database" )
-            echo "create DB"
-            createDB
-            break
-            ;;
-        "Use DataBase" )
-            echo "use dB"
-            break
-            ;;
-        "List Databases and Tables" )
-            echo "list db and tables"
-            break
-            ;;
-        * )
-            echo "Wrong Selection"
-            ;;
-    esac
-done
+showMenu(){
+    echo "Welcome to PLY database engine. Please Select an option";
+
+    select opt in "Create Database" "Use DataBase" "List Databases and Tables" "exit";
+    do
+        case $opt in
+            "Create Database" )
+                echo "create DB"
+                createDB
+                break
+                ;;
+            "Use DataBase" )
+                echo "use dB"
+                break
+                ;;
+            "List Databases and Tables" )
+                echo "list db and tables"
+                break
+                ;;
+            "exit" )
+                echo "Thank you for using PLY"
+                exit
+                break
+                ;;
+            * )
+                echo "Wrong Selection"
+                ;;
+        esac
+    done
+}
+
+showMenu
