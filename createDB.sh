6bc029bfe93f67136867a6a30fda275352a88c73
eslamkarim <eslamkarim@localhost.localdomain>
2020-01-24 14:09:32 +0200

    fixing directory issues on linux putting databases in /usr

--- createDB.sh
+++ createDB.sh
@@ -2,24 +2,25 @@
 
 
 createDB(){
-    if [[ -d "/c/Users/Eslam/Desktop/ITI/Bash/Ply/ply" ]]
+    if [[ -d "/usr/ply" ]]
     then
         echo "Please enter the Database name to create"
         read dbname
-        if [[ -d "/c/Users/Eslam/Desktop/ITI/Bash/Ply/ply/$dbname" ]]
+        if [[ -d "/usr/ply/$dbname" ]]
         then
             clear
             echo "sorry Database already exists"
             showMenu
         else
-            mkdir /c/Users/Eslam/Desktop/ITI/Bash/Ply/ply/$dbname
+            mkdir /usr/ply/$dbname
             clear
             echo "Database Created"
             showMenu
         fi
     else
         echo "making the dir this is only done for the first time"
-        mkdir ply
+        engine="/usr/ply"
+        mkdir $engine
         createDB
     fi
 } 
