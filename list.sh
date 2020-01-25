620feeebf3160fdf157a2a4b939ee53c5b151ef1
eslamkarim <eslamkarim@localhost.localdomain>
2020-01-25 12:23:13 +0200

    validate if there are databases or not

--- list.sh
+++ list.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 list(){
-    for db in `ls /usr/ply`
-    do
-        echo "database: $db"
-        if [ -z "$(ls -A -- "/usr/ply/$db")" ]; 
-        then
-            echo "Empty Database"
-        else
-            for table in `ls /usr/ply/$db`
-            do
-                echo "$table"
-            done
-        fi
-        echo "=============================================="
-    done
+    if [ -z "$(ls -A -- "/usr/ply")" ]; 
+    then
+        clear
+        echo "No Databases present"
+        showMenu
+    else
+        for db in `ls /usr/ply`
+        do
+            echo "database: $db"
+            if [ -z "$(ls -A -- "/usr/ply/$db")" ]; 
+            then
+                echo "Empty Database"
+            else
+                for table in `ls /usr/ply/$db`
+                do
+                    echo "$table"
+                done
+            fi
+            echo "=============================================="
+        done
+    fi
 }
